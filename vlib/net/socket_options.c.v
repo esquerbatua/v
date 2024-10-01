2bec26a0f2ea6549cd572984216bd8a8e9f1d6ac
Carlos Esquerdo Bernat <carlos@biteindustry.com>
2024-10-01 11:49:04 +0200

    fmt

--- vlib/net/socket_options.c.v
+++ vlib/net/socket_options.c.v
@@ -21,23 +21,23 @@ pub enum SocketOption {
 	debug            = C.SO_DEBUG
 	dont_route       = C.SO_DONTROUTE
 	error            = C.SO_ERROR
+	ip_proto_ipv6    = C.IPPROTO_IPV6
+	ipv6_only        = C.IPV6_V6ONLY
 	keep_alive       = C.SO_KEEPALIVE
 	linger           = C.SO_LINGER
 	oob_inline       = C.SO_OOBINLINE
-	reuse_addr       = C.SO_REUSEADDR
 	receive_buf_size = C.SO_RCVBUF
 	receive_low_size = C.SO_RCVLOWAT
 	receive_timeout  = C.SO_RCVTIMEO
+	reuse_addr       = C.SO_REUSEADDR
+	reuse_port       = C.SO_REUSEPORT // TODO make it work in windows
 	send_buf_size    = C.SO_SNDBUF
 	send_low_size    = C.SO_SNDLOWAT
 	send_timeout     = C.SO_SNDTIMEO
 	socket_type      = C.SO_TYPE
-	ipv6_only        = C.IPV6_V6ONLY
-	ip_proto_ipv6    = C.IPPROTO_IPV6
-	reuse_port       = C.SO_REUSEPORT // TODO make it work in windows
-	tcp_fastopen     = C.TCP_FASTOPEN // TODO make it work in windows
-	tcp_quickack     = C.TCP_QUICKACK // TODO make it work in os != linux
-	tcp_defer_accept = C.TCP_DEFER_ACCEPT // TODO make it work in windows
+	tcp_defer_accept = C.TCP_DEFER_ACCEPT // TODO make it work in os != linux
+	tcp_fastopen     = C.TCP_FASTOPEN     // TODO make it work in windows
+	tcp_quickack     = C.TCP_QUICKACK     // TODO make it work in os != linux
 }
 
 pub const opts_bool = [SocketOption.broadcast, .debug, .dont_route, .error, .keep_alive, .oob_inline]
